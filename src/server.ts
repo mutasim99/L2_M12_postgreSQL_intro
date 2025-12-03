ed50703121cd3188d8041bd2d0cda8b0c879aacc
mutasim99 <virginmr882@gmail.com>
2025-12-03 07:53:55 +0600

    create a user table on neon db

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,47 @@
 import express, { Request, Response } from 'express';
+import { Pool } from 'pg'
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req:Request, res:Response)=>{
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_2A4GKVrIkheU@ep-ancient-voice-adwqx4bg-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+});
+
+const initDb = async () => {
+    await pool.query(
+        `
+            CREATE TABLE If NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(150) NOT NULL,
+                age INT,
+                phone VARCHAR(15),
+                address TEXT,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+        `
+    )
+}
+
+initDb();
+
+app.get('/', (req: Request, res: Response) => {
     res.send('data not found')
 });
 
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'data is not posted'
+    })
 
-app.listen(port, ()=>{
+})
+
+
+app.listen(port, () => {
     console.log(`My app is running on port:${port}`);
-    
+
 })
